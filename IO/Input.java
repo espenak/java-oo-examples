e5be449748a3a2ff1a383fcf84f292035b8fbf3a
Tor Ivar Johansen <toij@luke.ifi.uio.no>
2011-01-26 16:53:42 +0100

    Changes in Input.java

--- IO/Input.java
+++ IO/Input.java
@@ -1,27 +1,11 @@
-package IO;
-
-/*
-1) Skriv om koden nedenfor slik at den leser fra tastatur istedenfor fra fil.
-2) Modifiser koden videre slik at navnene samles i en string separert med ':',
-   og skrives til skjerm når brukeren signaliserer at input er ferdig (ctrl-D).
-3) Modifiser den originale koden slik at den skriver til filen "names2.txt"
-   istedenfor til terminalen.
-*/
-
 import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.File;
 
-public class Input {
+public class Input1 {
     public static void main(String[] args) {
-        try {
-            Scanner s = new Scanner(new File("names.txt"));
-            while (s.hasNextLine()) {
-                String name = s.nextLine();
-                System.out.println("Name=" + name);
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            String name = s.nextLine();
+            System.out.println("Name=" + name);
         }
     }
 }
